ab3ad23d1e847f0d20a9b812ab843489191823b5
Leynier <leynier770@example.com>
2023-07-03 13:07:13 -0400

    haciendo lo de grafo en memoria

--- src/util/EdgeAux.java
+++ src/util/EdgeAux.java
@@ -1,10 +1,29 @@
 package util;
 
-public class EdgeAux {
+import java.io.Serializable;
+
+public class EdgeAux implements Serializable{
     private double weigth;
     private double posX;
     private double posY;
     private String address;
+    private int posCorner1;
+    private int posCorner2;
+    public int getPosCorner2() {
+        return posCorner2;
+    }
+
+    public void setPosCorner2(int posCorner2) {
+        this.posCorner2 = posCorner2;
+    }
+
+    public int getPosCorner1() {
+        return posCorner1;
+    }
+
+    public void setPosCorner1(int posCorner1) {
+        this.posCorner1 = posCorner1;
+    }
 
     public String getAddress() {
         return address;
